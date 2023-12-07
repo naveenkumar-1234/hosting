57e8e1d1a177f64754a256d0c89495bd109ced47
naveenkumar-1234 <iamnaveenkumar23@gmail.com>
2023-12-07 18:47:12 +0530

    changes added

--- src/Components/destination.jsx
+++ src/Components/destination.jsx
@@ -25,8 +25,6 @@ const destination = () => {
         <Grid picture={sky} price="$550.16" place="Paradise Beach, Bantayan Island" location="Rome, Italy" star="4.8"/>
         <Grid picture={boat} price="$550.16" place="Paradise Beach, Bantayan Island" location="Rome, Italy" star="4.8"/>
         <Grid picture={boat} price="$550.16" place="Paradise Beach, Bantayan Island" location="Rome, Italy" star="4.8"/>
-        <Grid picture={boat} price="$550.16" place="Paradise Beach, Bantayan Island" location="Rome, Italy" star="4.8"/>
-        <Grid picture={boat} price="$550.16" place="Paradise Beach, Bantayan Island" location="Rome, Italy" star="4.8"/>
         
       </div>
 
