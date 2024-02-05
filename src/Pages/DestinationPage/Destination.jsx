6102031a45cc6ad85abc945194f23a75d4e7b5a5
naveenkumar-1234 <iamnaveenkumar23@gmail.com>
2024-02-05 23:24:37 +0530

    bug fixed

--- src/Pages/DestinationPage/Destination.jsx
+++ src/Pages/DestinationPage/Destination.jsx
@@ -1,7 +1,7 @@
 import Slider from "./SliderComponent.jsx";
 const destination = () => {
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <div className=" block text-center lg:col-span-1 lg:gap-5">
         <div className="text-[#fa86b7] uppercase font-semibold py-6
         xl:text-left lg:px-28 lg:py-7 xl:text-4xl lg:3xl lg:text-center lg:text-4xl  tracking-widest
